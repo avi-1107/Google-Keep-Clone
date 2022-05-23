7b424239c0a791bf9cf6058af513042cbfe1c57a
Avi Dhingra <avi@Avis-MacBook-Pro.local>
2022-05-23 10:19:16 +0530

    Updates

---
+++ precache-manifest.d72c34a562ceebfb6881623dd15ef30a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8dd49f349b1806e756fdec460079e34",
+    "url": "/Google-Keep-Clone/index.html"
+  },
+  {
+    "revision": "8228be7a15990565f2a1",
+    "url": "/Google-Keep-Clone/static/js/2.82eb015c.chunk.js"
+  },
+  {
+    "revision": "6d04d577301a6dd4fb88",
+    "url": "/Google-Keep-Clone/static/js/main.340b9271.chunk.js"
+  },
+  {
+    "revision": "9aef967dab187bbc0222",
+    "url": "/Google-Keep-Clone/static/js/runtime-main.a5c1412d.js"
+  }
+]);
